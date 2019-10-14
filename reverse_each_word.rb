43e36505bc3f4b153c814c53630b3f5548880c4d
Luke Waring <lukewaring@gmail.com>
2019-10-14 17:03:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(sentence) do
+def reverse_each_word(sentence)
   sentence_array = sentence.split
   reverse_array = sentence_array.collect { |word| word.reverse }
   return reverse_array
